70e4660c14a9d71c836372c71cc26ea2d7935880
James Peach <jpeach@apache.org>
2019-11-13 07:53:23 +1100

    Handle more format cases.
    Rewrite the replacement to be character at a time so that we can
    handle '{{', '}}' and '{0}' format specifiers.

--- src/FmtFormatConversionCheck.cpp
+++ src/FmtFormatConversionCheck.cpp
@@ -33,6 +33,9 @@ void FmtFormatConversionCheck::registerMatchers(MatchFinder *Finder) {
       hasArgument(0, stringLiteral().bind("Arg0"))
     ).bind("Call");
 
+  // TODO(jpeach): this only matches string literals, so we miss cases where
+  // the format string is a named variable :(
+
   Finder->addMatcher(CallSiteMatcher, this);
 }
 
@@ -47,11 +50,7 @@ void FmtFormatConversionCheck::check(const MatchFinder::MatchResult &Result) {
 
   std::string Value = Range.printToString(*Result.SourceManager);
 
-  const char *Begin = SM.getCharacterData(Call->getArg(0)->getBeginLoc());
-  const char *End = SM.getCharacterData(Call->getArg(0)->getEndLoc());
-
-  llvm::errs() << "arg0 is '" << std::string(Begin, End) << "'\n";
-  llvm::errs() << "literal is '" << Arg0->getString() << "\n";
+  llvm::errs() << "literal is /" << Arg0->getString() << "/\n";
 
   if (!Call->getCallee()->getBeginLoc().isValid()) {
     llvm::errs() << "invalid callee begin loc " << Call->getCallee()->getBeginLoc().printToString(SM) << "\n";
@@ -61,47 +60,73 @@ void FmtFormatConversionCheck::check(const MatchFinder::MatchResult &Result) {
     llvm::errs() << "invalid callee end loc " << Call->getCallee()->getEndLoc().printToString(SM) << "\n";
   }
 
-  // Skip the fixit if the location is invalid.
+  // Skip the fixit if the location is invalid. IIUC, it would be
+  // invalid if this call doesn't map back to a specific source
+  // location.
   if (Call->getCallee()->getBeginLoc().isInvalid() ||
       Call->getCallee()->getEndLoc().isInvalid()) {
+    llvm::errs() << "skipping invalid call location\n";
     return;
   }
 
-  // Skip the fixit if it is in a macro expansion.
-  if (Arg0->getBeginLoc().isMacroID()) {
-    return;
-  }
+  // NOTE(jpeach): Some checks will call getBeginLoc.isMacroID() to
+  // avoid rewriting code that is in a macro expansion. However, if
+  // we do this, then most of the calls inside gtest test suites will
+  // be skipped since they are all in gtest macros.
+
+  // TODO(jpeach): Skip empty strings.
 
   using pos_t = std::string::size_type;
   std::string replacement = Arg0->getString();
 
   unsigned count = 0;
+  const pos_t end = replacement.size() + 1;
 
-  pos_t begin = 0;
-  pos_t end = replacement.size() + 1;
-
-  for (pos_t pos = replacement.find("{}", 0);
-        pos != std::string::npos;
-        pos = replacement.find("{}", pos + 1)) {
-    if ((pos > begin && replacement[pos-1] == '{') &&
-       (pos + 2 < end && replacement[pos+2] == '}')) {
-      // XXX it's not really {{}} that is special, it's that you double them to
-      // quote them, so {{ -> { and }} ->, which means that {{{}}} -> {$0}.
-      llvm::errs() << "skipping {{}} token\n";
-      ++pos;
+  for (pos_t lhs = 0; lhs < (end - 1); ++lhs) {
+    // Since we deal with paired '{}' below, we can just clean up
+    // escaped '}' here.
+    if (replacement[lhs] == '}' && replacement[lhs+1] == '}') {
+      replacement.replace(lhs, 2, std::string("}"));
       continue;
     }
 
-    // Replace the {} with $count.
-    replacement.replace(pos, 2, std::string("$") + std::to_string(count));
-    ++count;
-  }
+    if (replacement[lhs] != '{') {
+      continue;
+    }
 
-  for (pos_t pos = replacement.find("{{}}", 0);
-        pos != std::string::npos;
-        pos = replacement.find("{{}}", pos + 1)) {
-    // Replace the {{}} with {}.
-    replacement.replace(pos, 4, "{}");
+    if (replacement[lhs + 1] == '{') {
+      // This is a '{{', which is a quoted '{'. Replace it with '{'.
+      replacement.replace(lhs, 2, std::string("{"));
+      continue;
+    }
+
+    for (pos_t rhs = lhs + 1; rhs < end; ++rhs) {
+      if (replacement[rhs] >= '0' && replacement[rhs] <= '9') {
+        // We are still matching on "{[0-9]+}"
+        continue;
+      }
+
+      if (replacement[rhs] == '}') {
+        std::string num = replacement.substr(lhs + 1, rhs - lhs - 1);
+
+        if (num.empty()) {
+          // This is a '{}', replace it with the next counted argument.
+          replacement.replace(lhs, 2, std::string("$") + std::to_string(count));
+          ++count;
+        } else {
+          // This is a {N}, replace with $N.
+          replacement.replace(lhs, num.size() + 2, std::string("$") + num);
+        }
+
+        break;
+      }
+
+      // This is some fancy formatting we can't deal with punt it.
+      llvm::errs()
+        << "skipping unsupported format token /"
+        << replacement.substr(lhs, rhs - lhs)
+        << "/\n";
+    }
   }
 
   bool needsRaw = false;
@@ -110,13 +135,8 @@ void FmtFormatConversionCheck::check(const MatchFinder::MatchResult &Result) {
     needsRaw = true;
   }
 
-/*
-  for (pos_t pos = replacement.find("\n", 0);
-        pos != std::string::npos;
-        pos = replacement.find("\n", pos + 1)) {
-    replacement.replace(pos, 1, "\\n");
-  }
-*/
+  // TODO(jpeach): If this is a string literal with an embedded '\n',
+  // then we end up coverting it into a raw string, which sucks.
 
   // When we get a raw string literal, the parser strips the
   // prefix and suffix. Put it back if it looks like we will
@@ -127,12 +147,13 @@ void FmtFormatConversionCheck::check(const MatchFinder::MatchResult &Result) {
     replacement = "\"" + replacement + "\"";
   }
 
-  llvm::errs() << "replacement literal is '" << replacement << "\n";
+  llvm::errs() << "emitting replacement literal is /" << replacement << "/\n";
 
-  // Emit a warning and emit fixits to go from
-  //   x = fmt::Format("foo {} bar {}", ...)
-  // to
-  //   absl::Substitute("foo $0 bar $1", ...)
+  // XXX(jpeach): The replacements behave strangely with macro expansions.
+  // The warning output shows the range correctly, but the actual fixit
+  // change is applied to the wrong offsets as though the replacement
+  // range needs adjusting.
+
   diag(Call->getBeginLoc(), "call 'absl::Substitute' instead of 'fmt::format'")
       << FixItHint::CreateReplacement(
              CharSourceRange::getTokenRange(Call->getCallee()->getBeginLoc(), Call->getCallee()->getEndLoc()),
@@ -140,7 +161,6 @@ void FmtFormatConversionCheck::check(const MatchFinder::MatchResult &Result) {
       << FixItHint::CreateReplacement(
              CharSourceRange::getTokenRange(Arg0->getBeginLoc(), Arg0->getEndLoc()),
              replacement);
-      ;
 }
 
 }  // namespace abseil
